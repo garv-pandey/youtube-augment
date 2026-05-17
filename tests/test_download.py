0842bfbbe9606ceec01fbaaf6ac2533253fe53f1
Garv-Pandey <pandey.garv25@gmail.com>
2026-05-17 18:52:21 +0530

    fixed help string for downloading ffmpeg, deno and node and added their tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,3 +1,4 @@
+from os import path
 import pytest
 from unittest.mock import patch
 from ytaug.download import (
@@ -21,16 +22,32 @@ class TestCheckFfmpeg:
 
 class TestGetFfmpegInstallInstructions:
     def test_get_ffmpeg_install_instructions_windows(self):
-        pass
+        with patch("ytaug.download.platform.system", return_value="Windows"):
+            assert (
+                get_ffmpeg_install_instructions()
+                == "winget install ffmpeg \nor check their website: https://ffmpeg.org"
+            )
 
     def test_get_ffmpeg_install_instructions_macos(self):
-        pass
+        with patch("ytaug.download.platform.system", return_value="Darwin"):
+            assert (
+                get_ffmpeg_install_instructions()
+                == "brew install ffmpeg \nor check their website: https://ffmpeg.org"
+            )
 
     def test_get_ffmpeg_install_instructions_linux(self):
-        pass
+        with patch("ytaug.download.platform.system", return_value="Linux"):
+            assert (
+                get_ffmpeg_install_instructions()
+                == "sudo apt install ffmpeg (Debian/Ubuntu) \nor sudo dnf install ffmpeg (Fedora) \nor check their website: https://ffmpeg.org"
+            )
 
     def test_get_ffmpeg_install_instructions_undefined(self):
-        pass
+        with patch("ytaug.download.platform.system", return_value="unknown_abcd"):
+            assert (
+                get_ffmpeg_install_instructions()
+                == "check their website for download instructions \nhttps://ffmpeg.org"
+            )
 
 
 class TestGetJsRuntime:
@@ -46,16 +63,32 @@ class TestGetJsRuntime:
 
 class TestGetJsRuntimeInstallInstructions:
     def test_windows(self):
-        pass
+        with patch("ytaug.download.platform.system", return_value="Windows"):
+            assert (
+                get_js_runtime_install_instructions()
+                == "winget install deno  OR  winget install OpenJS.NodeJS \nor check their website: \nhttps://deno.com OR https://nodejs.org"
+            )
 
     def test_macos(self):
-        pass
+        with patch("ytaug.download.platform.system", return_value="Darwin"):
+            assert (
+                get_js_runtime_install_instructions()
+                == "brew install deno  OR  brew install node \nor check their website: \nhttps://deno.com OR https://nodejs.org"
+            )
 
     def test_linux(self):
-        pass
+        with patch("ytaug.download.platform.system", return_value="Linux"):
+            assert (
+                get_js_runtime_install_instructions()
+                == "curl -fsSL https://deno.land/install.sh | sh  OR  See https://nodejs.org/en/download/package-manager \nor check their website: \nhttps://deno.com OR https://nodejs.org"
+            )
 
     def test_unknown_os(self):
-        pass
+        with patch("ytaug.download.platform.system", return_value="unknown_abcd"):
+            assert (
+                get_js_runtime_install_instructions()
+                == "check their website for download instructions \nhttps://deno.com OR  See https://nodejs.org"
+            )
 
 
 class TestGetPlaylistInfoDlp:
